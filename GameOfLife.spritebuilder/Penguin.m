aa48da08b3c145519b2bc749d6b525b676f1eb0d
Clémence Coursimault <clemcoursi@MacBook-Pro-de-Clemence.local>
2014-06-26 09:38:01 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Penguin.m
@@ -0,0 +1,23 @@
+//
+//  Penguin.m
+//  GameOfLife
+//
+//  Created by Clémence Coursimault on 26/06/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
---
+++ GameOfLife.spritebuilder/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  GameOfLife
+//
+//  Created by Clémence Coursimault on 26/06/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
